eb99bd703bb832741eee7f044c2a7cb5e89c44e3
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-04-19 18:15:00 +0200

    Routing between pages added to navbar

--- react-hajp-webpage/src/App.js
+++ react-hajp-webpage/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js'
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './components/pages/MainPage';
+import BookingPage from './components/pages/BookingPage';
+import MembersPage from './components/pages/MembersPage';
+import ZepelinPage from './components/pages/ZepelinPage';
+import Gallery from './components/pages/Gallery';
+import PartyPage from './components/pages/PartyPage';
+import AboutPage from './components/pages/AboutPage';
 
 function App() {
   return (
     <div className="App">
-      <React.Fragment>
+      {/* <React.Fragment> */}
         <Navbar />
-      </React.Fragment>
+      {/* </React.Fragment> */}
+
+      <div className='main_container'>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/Booking' element={<BookingPage />} />
+          <Route path='/Members' element={<MembersPage />} />
+          <Route path='/Zepelin' element={<ZepelinPage />} />
+          <Route path='/Gallery' element={<Gallery />} />
+          <Route path='/Hajp' element={<PartyPage />} />
+          <Route path='/About' element={<AboutPage />} />
+        </Routes>
+      </div>
 
     </div>
   );
--- react-hajp-webpage/src/components/Navbar/Navbar.js
+++ react-hajp-webpage/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.css';
 import {FaBars, FaTimes, FaInstagram} from "react-icons/fa";
 import {useRef} from "react";
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 function Navbar() {
   const navRef = useRef();
@@ -14,13 +15,13 @@ function Navbar() {
     <header>
       <img src={require('../images/HajpLogo.png')} />
       <nav ref={navRef}>
-        <a href="#" data-replace="Početna"><span>Početna</span></a>
-        <a href="#" data-replace="Booking i Upisi"><span>Booking i Upisi</span></a>
-        <a href="#" data-replace="Treneri i Članovi"><span>Treneri i Članovi</span></a>
-        <a href="#" data-replace="Zepelin"><span>Zepelin</span></a>
-        <a href="#" data-replace="Galerija"><span>Galerija</span></a>
-        <a href="#" data-replace="Hajp Party"><span>Hajp Party</span></a>
-        <a href="#" data-replace="O nama"><span>O nama</span></a>
+        <Link to="/" data-replace="Početna"><span>Početna</span></Link>
+        <CustomLink to="/Booking" data-replace="Booking i Upisi"><span>Booking i Upisi</span></CustomLink>
+        <CustomLink to="/Members" data-replace="Treneri i Članovi"><span>Treneri i Članovi</span></CustomLink>
+        <CustomLink to="/Zepelin" data-replace="Zepelin"><span>Zepelin</span></CustomLink>
+        <CustomLink to="/Gallery" data-replace="Galerija"><span>Galerija</span></CustomLink>
+        <CustomLink to="/Hajp" data-replace="Hajp Party"><span>Hajp Party</span></CustomLink>
+        <CustomLink to="/About" data-replace="O nama"><span>O nama</span></CustomLink>
         <button className='nav-btn nav-close-btn' onClick={showNavbar}>
           <FaTimes />
         </button>
@@ -31,6 +32,17 @@ function Navbar() {
     </header>
   )
 
+}
+
+function CustomLink({to, children, ...props}){
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({path: resolvedPath.pathname, end: true})
+
+  return(
+    <Link className={isActive ? "active" : ""} to={to} {...props}>
+      {children}
+    </Link>
+  )
 }
 
 export default Navbar
