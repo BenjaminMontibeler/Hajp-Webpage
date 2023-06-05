9c29ae0a7980fc3f79cba81de76436347de71e2c
Benjamin Montibeler <benjaminmontibeler@gmail.com>
2023-06-05 18:45:55 +0200

    added about and booking page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { projectStorage } from '../config/firebase.js';
 import AOS from 'aos';
+import '../pages_styles/About.css'
 
 function About() {
-  return(
-    <h1>About</h1>
+
+  useEffect(() => {
+    const initAOS = setTimeout(() => {
+      AOS.init({
+        duration: 1000
+      });
+    }, 1000); // Delay of 1000 milliseconds (adjust as needed)
+
+    return () => clearTimeout(initAOS);
+  }, []);
+
+  return (
+    <div className='about-container container-fluid'>
+
+      <div className='info-container container-fluid'>
+        <div className='info-text' data-aos="fade-right" data-aos-offset="200" data-aos-once="true">
+          <h2>Kako je nastao Plesni Studio Hajp?</h2>
+          <p>Hajp je novonastali plesni studio osnovan prije od 3 godine od strane dvoje mladih plesača, trenera i studenata Josipa Bičćana i Karle Mitrović.
+            Josip i Karla bave se plesom više od 10 godina, dotakli su se mnogih plesnih stilova, sudjelovali u brojnim nastupima, natjecanjima,
+            koreografirali spotove te osnivali plesne evente. Hajp je sam po sebi počeo kao plesni event u lokalnom studentskom kafiću gdje su Karla i Josipa
+            osjetili zainteresiranost mladih studenata za plesom.
+            Danas se u plesnom studiju realiziraju i programi obrazovanja koje je verificiralo Ministarstvo znanosti, obrazovanja i sporta.</p>
+        </div>
+      </div>
+
+      <div className='info-container2 d-flex justify-content-center'>
+        <div className='info-text2' data-aos="fade-down" data-aos-offset="200" data-aos-once="true">
+          <h2>Plesni Stilovi</h2>
+          <p>Iako se hajp pretežito bavi urbanim plesnim stilovima poput Hip-hopa, Voguea, MTV-a i Break Dancea, imamo dvije grupe koje se bave
+            iskključivo suvremenim tj. Jazz danceom i Latino plesovima.</p>
+        </div>
+      </div>
+
+      <div className='info-container3 d-flex justify-content-between'>
+        <div className='row w-100 info-padding'>
+          <h2 data-aos="fade-right" data-aos-offset="200" data-aos-once="true">Dvorana</h2>
+          <div className='row w-100'>
+            <div className='col-6' data-aos="fade-right" data-aos-offset="200" data-aos-once="true">
+              <img src={require('../images/lokacija.JPG')} className='img-fluid'></img>
+            </div>
+            <div className='col-4 d-flex my-auto location-info' data-aos="fade-left" data-aos-offset="200" data-aos-once="true">
+              <div className='row'>
+                <p>Nalazimo se u Ul. Ivana Gundulića 5, Osijek gdje raspolažemo s jednom velikom te 3 manje dvorane; 2 urbane i baletnom</p>
+                <h3>Kontakt</h3>
+                <span>E-mail: hajp@gmail.com</span><br></br><br></br>
+                <span>Telefon: 095 698 6689</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import '../pages_styles/Booking.css'
+import AOS from 'aos';
 
 function Booking() {
+
+  useEffect(() => {
+    const initAOS = setTimeout(() => {
+      AOS.init({
+        duration: 1000
+      });
+    }, 1000); // Delay of 1000 milliseconds (adjust as needed)
+
+    return () => clearTimeout(initAOS);
+  }, []);
+
   return (
-    <div>
-      Booking
+    <div className='booking-container container-fluid'>
+      {/* <h1 className='booking-title'>Booking</h1> */}
+      <div className='row justify-content-evenly h-100'>
+        <div className='col booking-text h-100 d-flex align-items-center' data-aos="fade-right" data-aos-offset="200" data-aos-once="true">
+          <p>Tražite plesače sa nastup, spot, zabavu ili reklamu? Ili pak imate pitanja vezana za upise i termine.
+            Ovdje možete poslati upite! Pošaljite nam ime, prezime te e-mail i pobrinit ćemo se da vam odgovorimo u što kraćem roku.
+          </p>
+        </div>
+        <div className='col d-flex my-auto' data-aos="fade-left" data-aos-offset="200" data-aos-once="true">
+          <form className='row'>
+            <input type='text' placeholder='Ime i Prezime' required />
+            <input type='email' placeholder='E-mail' required />
+            <input type='text' placeholder='Predmet' required />
+            <textarea placeholder='Vaša poruka' required />
+            <button type='submit'>Pošalji</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
